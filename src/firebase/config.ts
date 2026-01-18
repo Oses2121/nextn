c5db3a2c9ef56ce02771fb35cb8d698242249729
Tb Moses <tambekema@gmail.com>
2026-01-18 01:07:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5370426623-16865",
+  "appId": "1:782114852630:web:b52dfaee0b36e7e8b0ca37",
+  "apiKey": "AIzaSyAIfSNfgm74RJY1fuoXLMS_4r2u7Qiph3c",
+  "authDomain": "studio-5370426623-16865.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "782114852630"
+};
